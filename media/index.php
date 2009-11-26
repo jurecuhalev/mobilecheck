00edfc66e6ad4b52d16e100bef5f5350b893800a
Jure Cuhalev <gandalfar@gmail.com>
2009-11-26 13:00:28 +0100

    developing away, simple statistics

---
+++ media/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="robots" content= "ALL" />
+<meta name="description" content= "Typogridphy &mdash; A Typographical and Grid Layout CSS Framework From Harry Roberts of CSS Wizardry" />
+<meta name="keywords" content= "" />
+<meta name="classification" content= "" />
+<meta name="author" content= "Harry Roberts - http://csswizardry.com" />
+<meta name="reply-to" content= "" />
+<meta name="copyright" content= "" />
+<link rel="shortcut icon" href="favicon.ico" />
+<title>Typogridphy &mdash; A Typographical and Grid Layout CSS Framework From Harry Roberts of CSS Wizardry</title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<!--[if IE]><link rel="stylesheet" type="text/css" href="ie.css" /><![endif]-->
+</head>
+
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<span id="nav"><a href="#" title="Nav Item Title">Nav Item One</a> &middot; <a href="#" title="Nav Item Title">Nav Item Two</a> &middot; <a href="#" title="Nav Item Title">Nav Item Three</a> &middot; <a href="#" title="Nav Item Title">Nav Item Four</a></span>
+		</div>
+		<div class="generic nineforty">
+			<h1>Typogridphy Demo Page &mdash; Use this page <span class="amp">&amp;</span> its grids as a basis for your typographical front-end development.</h1>
+		</div>
+		<div class="generic nineforty">
+			<h2>940px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem. Donec pretium lorem vel ligula. Fusce semper consequat dui. Integer ac mi. Cras faucibus nulla quis ipsum. Nunc augue turpis, tristique et, hendrerit id, hendrerit eu, lacus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vulputate. Proin et erat id ipsum accumsan porttitor. Suspendisse tellus arcu, scelerisque vel, feugiat sit amet, aliquet sed, velit.</p>
+		</div>
+		<div class="generic twotwenty">
+			<h2>220px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at.</p>
+		</div>
+		<div class="generic sevenhun end">
+			<h2>700px</h2>
+			<blockquote class="twoeighty right-float"><p>&ldquo;This is a blockquote, with a right float and a width of 280px. Even <em>this</em> maintains typographical flow!&rdquo; &mdash; <cite>Harry Roberts</cite></p></blockquote>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, <strong>est ligula egestas leo</strong>, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem. Donec pretium lorem vel ligula. Fusce semper consequat dui. Integer ac mi. Cras faucibus nulla quis ipsum. Nunc augue turpis, tristique et, hendrerit id, hendrerit eu, lacus.</p>
+		</div>
+		<div class="generic twoeighty break">
+			<h2>280px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		</div>
+		<div class="generic sixforty end">
+			<h2>640px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, <em>malesuada euismod turpis enim at arcu</em>. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem. Donec pretium lorem vel ligula. Fusce semper consequat dui. Integer ac mi. Cras faucibus nulla quis ipsum. Nunc augue turpis, tristique et, hendrerit id, hendrerit eu, lacus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vulputate. Proin et erat id ipsum accumsan porttitor. Suspendisse tellus arcu, scelerisque vel, feugiat sit amet, aliquet sed, velit.</p>
+		</div>
+		<div class="generic threeforty break">
+			<h2>340px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem.</p>
+		</div>
+		<div class="generic fiveeighty end">
+			<h2>580px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem. Donec pretium lorem vel ligula. Fusce semper consequat dui. Integer ac mi. Cras faucibus nulla quis ipsum. Nunc augue turpis, tristique et, hendrerit id, hendrerit eu, lacus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vulputate. Proin et erat id ipsum accumsan porttitor. Suspendisse tellus arcu, scelerisque vel, feugiat sit amet, aliquet sed, velit.</p>
+		</div>
+		<div class="generic fourhun break">
+			<h2>400px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		</div>
+		<div class="generic fivetwenty end">
+			<h2>520px</h2>
+			<img src="images/holder-01.gif" alt="A Placeholder Image" class="right-img" />
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris.</p>
+		</div>
+		<div class="generic foursixty break">
+			<h2>460px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris.</p>
+		</div>
+		<div class="generic foursixty end">
+			<h2>460px</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris.</p>
+		</div>
+		<div class="generic nineforty">
+			<h2>Lists&hellip;</h2>
+			<ol class="twotwenty left-float">
+				<li>This is an ordered list&hellip;</li>
+				<li>&hellip;which has been floated left.</li>
+				<li>It is also 22em wide&hellip;</li>
+				<li>Nested Lists:
+					<ul>
+						<li>This is a nested <span class="code">&lt;ul&gt;</span></li>
+					</ul>
+				</li>
+				<li>Nested Lists:
+					<ol>
+						<li>This is a nested <span class="code">&lt;ol&gt;</span></li>
+					</ol>
+				</li>
+				<li>&hellip;which equates to 220px when not zoomed.</li>
+			</ol>
+			<p><strong>Lorem</strong> ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem. Donec pretium lorem vel ligula. Fusce semper consequat dui. Integer ac mi. Cras faucibus nulla quis ipsum. Nunc augue turpis, tristique et, hendrerit id, hendrerit eu, lacus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vulputate. Proin et erat id ipsum accumsan porttitor. Suspendisse tellus arcu, scelerisque vel, feugiat sit amet, aliquet sed, velit. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+			<ul class="foursixty left-float">
+				<li>This is an unordered list&hellip;</li>
+				<li>&hellip;which has been floated left.</li>
+				<li>It is also 46em wide&hellip;</li>
+				<li>&hellip;which equates to 460px when not zoomed.</li>
+				<li>Nested Lists:
+					<ul>
+						<li>This is a nested <span class="code">&lt;ul&gt;</span></li>
+					</ul>
+				</li>
+				<li>Nested Lists:
+					<ol>
+						<li>This is a nested <span class="code">&lt;ol&gt;</span></li>
+					</ol>
+				</li>
+				<li>The wider the class assigned, the wider the list will be &mdash; pretty nifty eh?.</li>
+			</ul>
+			<p><strong>Lorem</strong> ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede <a href="#" title="Always try and give links a title attribute">euismod pede</a>, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam suscipit, quam at placerat interdum, quam turpis placerat pede, eget sagittis arcu sapien vel mauris. Nulla faucibus turpis eget tellus. Integer mattis dapibus lorem. Donec pretium lorem vel ligula. Fusce semper consequat dui. Integer ac mi. Cras faucibus nulla quis ipsum. Nunc augue turpis, tristique et, hendrerit id, hendrerit eu, lacus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vulputate. Proin et erat id ipsum accumsan porttitor. Suspendisse tellus arcu, scelerisque vel, feugiat sit amet, aliquet sed, velit.</p>
+		</div>
+		<div class="generic threeforty">
+			<h3>Images</h3>
+			<img src="images/holder-01.gif" alt="A Placeholder Image" class="left-img" />
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. In accumsan diam vitae velit. Aliquam vehicula, turpis sed egestas porttitor, est ligula egestas leo, at interdum leo ante ut risus. Cras ut nunc. Phasellus imperdiet, urna in volutpat venenatis, mauris pede euismod pede, malesuada euismod turpis enim at arcu. Sed placerat accumsan mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+		</div>
+		<div class="generic twotwenty">
+			<h3>Glyphs</h3>
+			<p class="intro drop"><strong>Typogridphy</strong> is also designed to use glyphs, as opposed to the normal text used in web design. For example, use &lsquo;&aelig;&rsquo; in place of &lsquo;ae&rsquo;, &lsquo;&mdash;&rsquo; instead of &lsquo;-&rsquo; and &lsquo;&hellip;&rsquo; as opposed to &lsquo;...&rsquo;.</p>
+			<p>A good way of finding out about different glyphs is through Dreamweaver&rsquo;s design view. Wow, I never thought I&rsquo;d be singing the praises of a <abbr title="What You See Is What You Get">WYSIWYG</abbr> application!</p>
+		</div>
+		<div class="generic threeforty end">
+			<h3>Headings</h3>
+			<h1>Heading 1</h1>
+			<h2>Heading 2</h2>
+			<h3>Heading 3</h3>
+			<h4>Heading 4</h4>
+			<h5>Heading 5</h5>
+			<h6>Heading 6</h6>
+		</div>
+		<div class="generic nineforty">
+			<p id="footer-nav"><a href="http://csswizardry.com/" title="CSS Wizardry &mdash; Home of Harry Roberts">&copy; MMVIII Harry Roberts</a> &middot; <a href="#" title="Nav Item Title">Nav Item One</a> &middot; <a href="#" title="Nav Item Title">Nav Item Two</a> &middot; <a href="#" title="Nav Item Title">Nav Item Three</a> &middot; <a href="#" title="Nav Item Title">Nav Item Four</a></p>
+		</div>
+	</div>
+</body>
+</html>
